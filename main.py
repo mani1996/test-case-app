b26fe7ee604a942618645fedbff9aa5ebdb96432
Manikantan Narasimhan <rvns03@gmail.com>
2015-09-02 15:46:55 +0530

    Added exceptions to main.py,removed print statements.

--- main.py
+++ main.py
@@ -3,17 +3,10 @@ import os,sys,shutil
 class NewTestCase(object):
 	def __init__(self,folder_name):
 		self.ip_count = self.op_count = 0 #Number of input files,output files present till now
-		if(os.path.exists(folder_name)):
-			ch = raw_input('Folder already exists.\nEnter "y" to delete existing folder and proceed:')
-			if(ch=='y'):
-				shutil.rmtree(folder_name,ignore_errors=True)
-				os.mkdir(folder_name)
+		if('/' not in folder_name and folder_name!=''):
+			if(os.path.exists(folder_name)):
+				raise Exception('Folder with same name already there')
 			else:
-				self.ip_count = self.op_count = -1 #Invalid, basically
+				os.mkdir(folder_name)
 		else:
-			os.mkdir(folder_name)
-
-
-			
-
-
+			raise Exception('Improper file name!')
