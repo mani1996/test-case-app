4c7bd5a0a0b71f39857ddbfba6192bd63148e438
Manikantan Narasimhan <rvns03@gmail.com>
2015-09-14 01:10:23 +0530

    The file which generates inputs from arguments of NewInput method

--- Example/Square/Generator.py
+++ Example/Square/Generator.py
@@ -1,3 +1,4 @@
+#The file which generates inputs based on parameters from tcmanager.NewTestSuite.NewInput method
 import sys,random
 params = sys.argv[1:] # First argument of sys.argv contains the filename('Generator.py')
 t,l,u = map(lambda x:int(x),params)
